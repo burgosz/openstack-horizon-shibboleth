f5469aa959e77a406b788647d1811bda8389bf9c
81871@bme.hu <burgosz@gmail.com>
2015-01-21 12:21:59 +0100

    openstack-dashboard modifications

---
+++ openstack-dashboard/urls.py
@@ -0,0 +1,58 @@
+# Copyright 2012 United States Government as represented by the
+# Administrator of the National Aeronautics and Space Administration.
+# All Rights Reserved.
+#
+# Copyright 2012 Nebula, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+"""
+URL patterns for the OpenStack Dashboard.
+"""
+
+from django.conf import settings
+from django.conf.urls import include  # noqa
+from django.conf.urls import patterns
+from django.conf.urls.static import static  # noqa
+from django.conf.urls import url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns  # noqa
+import openstack_auth_shib
+import horizon
+
+
+urlpatterns = patterns('',
+    url(r'', include(horizon.urls))
+)
+if settings.AUTHENTICATION_BACKENDS[0]=='openstack_auth_shib.backend.Shib_KeystoneBackend':
+	urlpatterns += patterns('',
+		  url(r'^auth/', include('openstack_auth_shib.urls')),
+		  url(r'^$', 'openstack_auth_shib.views.login', name='login'),
+	)
+else:
+	urlpatterns += patterns('',
+		  url(r'^auth/', include('openstack_auth.urls')),
+		  url(r'^$', 'openstack_dashboard.views.splash', name='splash'),
+	)
+
+# Development static app and project media serving using the staticfiles app.
+urlpatterns += staticfiles_urlpatterns()
+
+# Convenience function for serving user-uploaded media during
+# development. Only active if DEBUG==True and the URL prefix is a local
+# path. Production media should NOT be served by Django.
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        url(r'^500/$', 'django.views.defaults.server_error')
+    )
