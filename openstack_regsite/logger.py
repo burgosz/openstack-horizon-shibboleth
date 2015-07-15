340d4cb8eccea5b9368d50ac97d956dcd6585cfb
burgosz <tenczer.szabolcs@mta.sztaki.hu>
2015-07-15 10:33:28 +0200

    forget gitignore before pushing

--- openstack_regsite/logger.py
+++
@@ -1,7 +0,0 @@
-from datetime import datetime, date
-__author__ = 'burgosz'
-
-def log(message):
-    logfile = open('log', 'a+')
-    logfile.write("{:%H:%M:%d}: ".format(datetime.now()))
-    logfile.write("Message: %s.\n" % message)
