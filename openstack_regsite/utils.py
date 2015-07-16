356541eab8cd3e4fd3bd2872270752f726f2e365
burgosz <tenczer.szabolcs@mta.sztaki.hu>
2015-07-16 10:11:39 +0200

    some cleanup

--- openstack_regsite/utils.py
+++ openstack_regsite/utils.py
@@ -18,10 +18,9 @@ def admin_client():
     client = keystone_client.Client(token=admin_token, endpoint=endpoint)
     return client
 
-def get_user(request, username):
+def get_user(username):
     client = admin_client()
     userlist = client.users.list()
-
     for user in userlist:
         if user.name == username:
             return user
@@ -47,17 +46,14 @@ def get_entitlemets(request):
     entitlement = request.META.get(entitlement_field, '').replace("\\", "")
     entitlement_list = entitlement.split(';')
     ent_roles = defaultdict(list)
-
     # retrieve info from shibboleth session
     for entitlement in entitlement_list:
         splitted = entitlement.split(":")
         rolename = splitted[len(splitted)-1]
         tenantname = splitted[len(splitted)-2]
         ent_roles[tenantname].append(rolename)
-
     LOG.debug("entitlement = %s" % entitlement)
     LOG.debug("ent_roles = %s" % ent_roles)
-
     return ent_roles
 
 def create_tenants(client, tenant_list):
@@ -66,8 +62,7 @@ def create_tenants(client, tenant_list):
         tenant = get_tenant(t)
         if tenant is None:
             LOG.info("Creating tenant %s." % t)
-            tenants = client.projects.create(name=t, domain=default_domain)
-
+            client.projects.create(name=t, domain=default_domain)
         new_tenants.append(tenant)
     return new_tenants
 
@@ -79,16 +74,12 @@ def create_roles(client, role_list):
 
 def update_roles(request, user):
     client = admin_client()
-
     ent_roles = get_entitlemets(request)
-    new_tenants = create_tenants(client, ent_roles.keys())
-
+    create_tenants(client, ent_roles.keys())
     role_list = [ent_roles[r] for r in ent_roles.keys()]
     role_list = reduce(lambda x, y: x+y, role_list)
     create_roles(client, role_list)
-
     existing_tenants = client.projects.list()
-
     for tenant in existing_tenants:
         # Check roles for the current tenant
         for role in client.roles.list(user=user, project=tenant):
@@ -102,7 +93,6 @@ def update_roles(request, user):
             # from the user.
             else:
                 client.roles.revoke(role, user=user, project=tenant)
-
     # Create remaining roles in the ent_roles dict.
     for tenant in existing_tenants:
         if tenant.name in ent_roles:
@@ -112,28 +102,21 @@ def update_roles(request, user):
 
 def update_mail(user, email):
     client = admin_client()
-    
     client.users.update(user, email=email)
 
-
-def create_user(request, username):
+def create_user(username):
     client = admin_client()
     newuser = client.users.create(name=username, domain=default_domain)
-
     return newuser
 
 def update_user(request):
     username = request.META.get(name_field, None)
-    user = get_user(request, username)
-
+    user = get_user(username)
     if user is None:
         LOG.info("Creating user %s." % username)
-        user = create_user(request, username)
-
+        user = create_user(username)
     update_roles(request, user)
-    
     email = request.META.get(mail_field, None)
     if user.email != email:
         update_mail(user, email)
-
     return user.name
