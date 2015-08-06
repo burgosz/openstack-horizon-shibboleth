addeb7a0de4f1a0b26d60bc5b6222fc4f65de69b
Andrea Biancini <andrea.biancini@gmail.com>
2015-08-06 15:56:45 +0200

    Revisited django site

--- openstack_regsite/urls.py
+++ openstack_regsite/urls.py
@@ -1,7 +1,12 @@
-from django.conf.urls import patterns, include, url
 from django.contrib import admin
-import openstack_regsite
+from django.conf.urls import patterns, include, url
+from django.conf.urls.static import static
+from django.conf import settings
 
-urlpatterns = patterns('openstack_regsite.views',
-    url(r'^$', 'shib_hook'),
+
+urlpatterns = patterns('views',
+    url(r'^$', 'index'),
+    url(r'^shib_hook$', 'shib_hook'),
 )
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- openstack_regsite/views.py
+++ openstack_regsite/views.py
@@ -1,16 +1,34 @@
-from django.http import HttpResponse
 from os import environ
+
+from django.http import HttpResponse
 from django.shortcuts import redirect
 from django.conf import settings
-import requests, json
-from openstack_regsite import utils
-from openstack_regsite import logger
+from django.shortcuts import render_to_response
 
+import requests
+import json
+
+import utils
+import logging
+
+logger = logging.getLogger('openstack_regsite')
+
+def index(request):
+
+    attributes = {
+        'openstack_servername': settings.OPENSTACK_NAME,
+        'openstack_url': settings.OPENSTACK_URL,
+        'openstack_keystone': settings.OPENSTACK_KEYSTONE_ADMIN_URL,
+        'shibboleth_name': request.META.get(settings.SHIBBOLETH_NAME_ATTRIBUTE, None),
+        'shibboleth_entitlement': request.META.get(settings.SHIBBOLETH_ENTITLEMENT_ATTRIBUTE, None),
+    }
+
+    return render_to_response('regsite/index.html', attributes)
 
 def shib_hook(request):
     eppn = request.META.get(settings.SHIBBOLETH_NAME_ATTRIBUTE, None)
     if not eppn:
-        logger.log("Name attribute is missing!")
+        logger.error("Name attribute is missing!")
         raise Exception("Missing name attribute.")
 
     entitlement = request.META.get(settings.SHIBBOLETH_ENTITLEMENT_ATTRIBUTE, None)
