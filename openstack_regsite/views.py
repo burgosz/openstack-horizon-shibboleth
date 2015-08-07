72cf4512117246b849c1b934f176f55085b46e01
Andrea Biancini <andrea.biancini@gmail.com>
2015-08-07 14:44:37 +0200

    Parametrized user acceptance for creation

--- openstack_regsite/views.py
+++ openstack_regsite/views.py
@@ -2,8 +2,8 @@ from os import environ
 
 from django.http import HttpResponse
 from django.shortcuts import redirect
-from django.conf import settings
 from django.shortcuts import render_to_response
+from django.conf import settings
 
 import requests
 import json
@@ -13,23 +13,7 @@ import logging
 
 logger = logging.getLogger('openstack_regsite')
 
-def index(request):
-    eppn = request.META.get(settings.SHIBBOLETH_NAME_ATTRIBUTE, None)
-    if utils.user_exists(eppn):
-        return redirect(next_page)
-    else:
-        attributes = {
-            'openstack_servername': settings.OPENSTACK_NAME,
-            'openstack_url': settings.OPENSTACK_URL,
-            'openstack_keystone': settings.OPENSTACK_KEYSTONE_ADMIN_URL,
-            'shibboleth_name': eppn,
-            'shibboleth_entitlement': dict(utils.get_entitlemets(request)),
-            'next_page': request.GET.get('return', '/'),
-        }
-
-        return render_to_response('regsite/index.html', attributes)
-
-def shib_hook(request):
+def _update_user(request):
     next_page = request.GET.get('return', '/')
     eppn = request.META.get(settings.SHIBBOLETH_NAME_ATTRIBUTE, None)
     if not eppn:
@@ -42,3 +26,27 @@ def shib_hook(request):
 
     #redirect to the Shibboleth HOOK return url.
     return redirect(next_page)
+
+def _show_user_creation_page(request):
+    attributes = {
+        'openstack_servername': settings.OPENSTACK_NAME,
+        'openstack_url': settings.OPENSTACK_URL,
+        'openstack_keystone': settings.OPENSTACK_KEYSTONE_ADMIN_URL,
+        'shibboleth_name': request.META.get(settings.SHIBBOLETH_NAME_ATTRIBUTE, None),
+        'shibboleth_entitlement': dict(utils.get_entitlemets(request)),
+        'return': request.GET.get('return', '/'),
+        'target': request.GET.get('target', None),
+        'base_url': settings.BASE_URL,
+    }
+
+    return render_to_response('regsite/index.html', attributes)
+
+def index(request):
+    eppn = request.META.get(settings.SHIBBOLETH_NAME_ATTRIBUTE, None)
+    if not settings.USER_ACCEPT_CREATION or utils.user_exists(eppn):
+        return _update_user(request)
+    else:
+        return _show_user_creation_page(request)
+
+def shib_hook(request):
+    return _update_user(request)
